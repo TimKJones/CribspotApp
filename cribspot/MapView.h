e91d6c2647c42b31aa4f07dd94eb88c839ff7d01
Patrick Wilson <pdubslax@umich.edu>
2013-10-24 15:57:56 -0400

    Second Commit

---
+++ cribspot/MapView.h
@@ -0,0 +1,13 @@
+//
+//  MapView.h
+//  cribspot
+//
+//  Created by Patrick Wilson on 10/24/13.
+//  Copyright (c) 2013 Patrick Wilson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapView : UIViewController
+
+@end
